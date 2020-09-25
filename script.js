cf5c6a9addd52d82c8a1602ac684c8b0cbeb8dcd
muhdarifrawi <muhdarifrawi@gmail.com>
2020-09-25 12:46:12 +0800

    Added the following: - created empty array (named as yearly) to store yearly returns. - function to handle first year return. - function to handle the subsequent returns     (will fetch from array to see previous returns). - if statement to reset 'yearly' array to zero. - resolved minor bug where it would return NaN when monthly contribution     is set to 300.

--- script.js
+++ script.js
@@ -9,32 +9,99 @@ function check(){
     console.log(returns);
 }
 
+let yearly = [];
 
 function calculate(){
+    console.log("calc success")
     let notification = document.getElementById("notification");
     let periods = document.getElementsByName("period");
+    let period = 0;
     for(i=0;i<periods.length;i++){
         if(periods[i].checked){
-            period = periods[i].value;
+            period = parseInt(periods[i].value);
         }
     }
     let investYears = document.getElementById("investYears").value; 
     let mthContribute = document.getElementById("mthContribute").value; 
     let returns = document.getElementById("returns").value;
     
-    console.log(returns);
-    if (period=="" || mthContribute=="" || investYears==""){
-        notification.innerHTML = "<h3>Please fill all fields</h3>"    
+    console.log(period);
+    if(yearly.length!=0){
+        yearly = [];
     }
-    else if(investYears < 1 || investYears > 99){
-        notification.innerHTML = "<h3>Please enter years to invest from 1 to 99 years</h3>" 
-        console.log("invest value error")
+    if(investYears < period || investYears > 99){
+        notification.innerHTML = `<h3>Please enter years to invest from ${period} to 99 years</h3>`
+        return console.log("invest value error")
     }
-    else if(age < 21 || age > 99){
-        notification.innerHTML = "<h3>Please enter age between 21 to 99</h3>" 
-        console.log("age value error")
+    else if(mthContribute < 300){
+        notification.innerHTML = "<h3>Please enter a minimum value of 300 for monthly contributions</h3>" 
+        return console.log("invest value error")
     }
     else{
-        notification.innerHTML = `<h3>${amount}</h3>`   
+        returns = (returns/100) + 1;
+        firstYear(mthContribute,period,returns);
+        // console.log(amount);
+        nthYear(investYears,mthContribute,period,returns);
+        notification.innerHTML = `<h3>${yearly[0]}</h3>`
+        console.log(yearly);   
     }
 }
+
+function firstYear(mthContribute, period, returns){
+    console.log("success");
+    let yrContribute = mthContribute * 12
+    let welcomeBonus = 0;
+    // console.log("returns conversion:", returns);
+    if(period == 10){
+        if (yrContribute >= 3600 && yrContribute < 120000){
+            welcomeBonus = 1.10;
+            let fyReturns = yrContribute*welcomeBonus*0.975*returns;
+            console.log(fyReturns, welcomeBonus);
+            yearly.push(fyReturns);
+            return;
+        }
+        else if (yrContribute >= 120000){
+            welcomeBonus = 1.40;
+            let fyReturns = yrContribute*welcomeBonus*0.975*returns;
+            // console.log(fyReturns, welcomeBonus);
+            yearly.push(fyReturns);
+            return;
+        }
+        else{
+            console.log("yearly contribute error");
+        }
+    }
+    else if(period == 20){
+        if (yrContribute > 2400 && yrContribute < 96000){
+            welcomeBonus = 1.30;
+            let fyReturns = yrContribute*welcomeBonus*0.975*returns;
+            // console.log(fyReturns, welcomeBonus);
+            yearly.push(fyReturns);
+            return;
+        }
+        else if (yrContribute >= 96000){
+            welcomeBonus = 1.60;
+            let fyReturns = yrContribute*welcomeBonus*0.975*returns;
+            // console.log(fyReturns, welcomeBonus);
+            yearly.push(fyReturns);
+            return;
+        }
+        else{
+            console.log("yearly contribute error");
+        }
+    }
+    else{
+        console.log("unexpected error");
+    }
+}
+
+function nthYear(investYears,mthContribute,returns){
+    let nth = investYears-1;
+    let yrContribute = mthContribute * 12
+    for(i=0;i<nth;i++){
+        let nthReturns = yearly[i]*yrContribute*0.975*returns;
+        yearly.push(nthReturns);
+    }
+
+}
+

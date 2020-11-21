5a87c9ef904055fdc518275c62f88048c0fe6579
muhdarifrawi <muhdarifrawi@gmail.com>
2020-11-21 11:38:35 +0800

    Added input validation - moved investment period check into its own function. - input validation is holding on to variables for years to invest and     monthly contribution.

--- script-2.js
+++ script-2.js
@@ -1,4 +1,4 @@
-document.getElementById("calculate").setAttribute("onclick","calculate()");
+document.getElementById("calculate").setAttribute("onclick","inputValidation()");
 document.getElementById("returns").setAttribute("onchange","returns()");
 // sets initial returns display to 6%
 document.getElementById("returnsValue").innerText = "6%";
@@ -9,18 +9,8 @@ function returns(){
     document.getElementById("returnsValue").innerText = returns + "%";
 }
 
-function calculate(){
-   
-    // get years of investment 
-    let investYears = document.getElementById("investYears").value;
-    // get monthly contributions
-    let mthContribute = document.getElementById("mthContribute").value;
-    // check on the welcome bonus
-    welcomeBonus(mthContribute);
-
-}
-
-function welcomeBonus(mthContribute){
+function checkPeriod(){
+    // checks the investment policy, 10yrs or 20yrs
     let periods = document.getElementsByName("period");
     let period = 0;
     for(i=0;i<periods.length;i++){
@@ -28,16 +18,54 @@ function welcomeBonus(mthContribute){
             period = parseInt(periods[i].value);
         }
     }
-    if (mthContribute < 1000 && period == 10){
+    return period;
+}
+
+function inputValidation(){
+    // get years of investment 
+    let investYears = document.getElementById("investYears").value;
+    // get monthly contributions
+    let mthContribute = document.getElementById("mthContribute").value;
+    if(investYears < checkPeriod() || investYears > 99){
+        notification.innerHTML = `<h3>Please enter years to invest from ${checkPeriod()} to 99 years</h3>`
+        return console.log("invest value error")
+    }
+    else if(checkPeriod() == 10 && mthContribute < 300){
+        notification.innerHTML = "<h3>Please enter a minimum value of 300 for monthly contributions</h3>" 
+        return console.log("invest value error")
+    }
+    else if(checkPeriod() == 20 && mthContribute < 200){
+        notification.innerHTML = "<h3>Please enter a minimum value of 300 for monthly contributions</h3>" 
+        return console.log("invest value error")
+    }
+    else{
+        console.log("input validation pass");
+        calculate(investYears,mthContribute);
+    }
+}
+
+// mC is month contribute
+// iY is investYears
+function calculate(iY,mC){
+   
+    
+    
+    // check on the welcome bonus
+    welcomeBonus(mC);
+
+}
+
+function welcomeBonus(mC){
+    if (mC < 1000 && checkPeriod() == 10){
         console.log("10%");
     }
-    else if (mthContribute >= 1000 && period == 10){
+    else if (mC >= 1000 && checkPeriod() == 10){
         console.log("40%");
     }
-    else if (mthContribute < 800 && period == 20){
+    else if (mC < 800 && checkPeriod() == 20){
         console.log("30%");
     }
-    else if (mthContribute >= 800 && period == 20){
+    else if (mC >= 800 && checkPeriod() == 20){
         console.log("60%");
     }
     else{

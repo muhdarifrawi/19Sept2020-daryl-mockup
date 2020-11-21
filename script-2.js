bccac78e3539ce50f21b8f0c1889656d6c8d88d4
muhdarifrawi <muhdarifrawi@gmail.com>
2020-11-21 13:58:04 +0800

    added and checked formulas - decided to not round the numbers to be stored. - instead numbers would be left as is and users are only shown     the rounded numbers. - calculations seems to be in working order as of now.

--- script-2.js
+++ script-2.js
@@ -7,6 +7,7 @@ function returns(){
     let returns = document.getElementById("returns").value;
     // changes the returns display as per slider's value
     document.getElementById("returnsValue").innerText = returns + "%";
+    return returns;
 }
 
 function checkPeriod(){
@@ -28,15 +29,15 @@ function inputValidation(){
     let mthContribute = document.getElementById("mthContribute").value;
     if(investYears < checkPeriod() || investYears > 99){
         notification.innerHTML = `<h3>Please enter years to invest from ${checkPeriod()} to 99 years</h3>`
-        return console.log("invest value error")
+        return console.log("invest value error");
     }
     else if(checkPeriod() == 10 && mthContribute < 300){
         notification.innerHTML = "<h3>Please enter a minimum value of 300 for monthly contributions</h3>" 
-        return console.log("invest value error")
+        return console.log("invest value error");
     }
     else if(checkPeriod() == 20 && mthContribute < 200){
         notification.innerHTML = "<h3>Please enter a minimum value of 300 for monthly contributions</h3>" 
-        return console.log("invest value error")
+        return console.log("invest value error");
     }
     else{
         console.log("input validation pass");
@@ -47,26 +48,48 @@ function inputValidation(){
 // mC is month contribute
 // iY is investYears
 function calculate(iY,mC){
-   
+    let yearly = [];
+    let totalInvested = 0;
+    for (count=1;count<=iY;count++){
+        if (count==1){
+            totalInvested = (mC*12) * welcomeBonus(mC) * 0.975 * (returns()/100+1);
+            yearly.push(totalInvested);
+            console.log(`year ${count}`,totalInvested.toFixed(2));
+        }
+        else if (count<=10){
+            totalInvested = (yearly[count-2]+(mC*12)) * 0.975 * (returns()/100+1);
+            yearly.push(totalInvested);
+            console.log(`year ${count}`,totalInvested.toFixed(2));
+        }
+        else{
+            totalInvested = (yearly[count-2]+(mC*12)) * 0.996 * (returns()/100+1);
+            yearly.push(totalInvested);
+            console.log(`year ${count}`,totalInvested.toFixed(2));
+        }
+    }
     
     
     // check on the welcome bonus
-    welcomeBonus(mC);
+    // welcomeBonus(mC);
 
 }
 
 function welcomeBonus(mC){
     if (mC < 1000 && checkPeriod() == 10){
         console.log("10%");
+        return 1.1;
     }
     else if (mC >= 1000 && checkPeriod() == 10){
         console.log("40%");
+        return 1.4;
     }
     else if (mC < 800 && checkPeriod() == 20){
         console.log("30%");
+        return 1.3;
     }
     else if (mC >= 800 && checkPeriod() == 20){
         console.log("60%");
+        return 1.6;
     }
     else{
         console.log("welcomeBonus unexpected error");
